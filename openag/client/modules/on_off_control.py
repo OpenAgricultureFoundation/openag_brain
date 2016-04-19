62483c4753eab1c0ef534ccf305ef8b5e2f43cb7
Leon Chambers <leon8556@gmail.com>
2016-04-19 17:10:18 -0400

    Updated control module to cast everything to floats

--- openag/client/modules/on_off_control.py
+++ openag/client/modules/on_off_control.py
@@ -6,15 +6,15 @@ class OnOffControl(Module):
     state = Output(InternalVariable.BOOLEAN)
 
     def init(self, default: "The default value for the set point"):
-        self.current_set_point = default
-        self.current_measured = default
+        self.current_set_point = float(default)
+        self.current_measured = float(default)
 
     def on_set_point(self, item):
-        self.current_set_point = item.value
+        self.current_set_point = float(item.value)
         self.compute_state()
 
     def on_measured(self, item):
-        self.current_measured = item.value
+        self.current_measured = float(item.value)
         self.compute_state()
 
     def compute_state(self):
