c20a5483973029fde91790f7c85086abe4886bf0
Leon Chambers <leon8556@gmail.com>
2016-04-19 14:45:55 -0400

    Fixed typo

--- openag/client/core/init_db.py
+++ openag/client/core/init_db.py
@@ -8,5 +8,3 @@ if __name__ == '__main__':
             server.create(db_name.value)
         except PreconditionFailed:
             pass
-
-    # TODO: Create variable entries too
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 
-here = path.abspath(path.dirname(__file__))o
+here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
