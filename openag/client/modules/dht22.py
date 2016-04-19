f302a868bffc562abfb2100bc8d0f6f62618ecea
Leon Chambers <leon8556@gmail.com>
2016-04-19 15:37:39 -0400

    Made all arguments on dht22 module keyword arguments

--- openag/client/modules/dht22.py
+++ openag/client/modules/dht22.py
@@ -5,9 +5,10 @@ class Dht22(Module):
     humidity = Output(EnvironmentalVariable.HUMIDITY)
 
     def init(self, board_id: "ID of the module for the board to which this " \
-            "sensor is connected", temp_id: "CBAN id of the temperature point",
-            hum_id: "CBAN id of the humitdity point", update_interval:
-            "Frequency with which to poll this sensor"):
+            "sensor is connected" = None, temp_id: "CBAN id of the" \
+            "temperature point" = None, hum_id: "CBAN id of the humitdity " \
+            "point" = None, update_interval: "Frequency with which to poll " \
+            "this sensor" = 1):
         self.board = self.ask(board_id)
         self.temp_id = temp_id
         self.hum_id = hum_id
