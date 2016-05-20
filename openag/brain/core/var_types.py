e8b1408c513bc6f718dc9cb54233519126881c33
Leon Chambers <leon8556@gmail.com>
2016-05-20 14:53:59 -0400

    Created am2315 module and modified modules to work with updated openag-pymata-aio

--- openag/brain/core/var_types.py
+++ openag/brain/core/var_types.py
@@ -16,7 +16,7 @@ class InternalVariable(Enum):
 
 class EnvironmentalVariable(Enum):
     AIR_TEMPERATURE = 'air_temperature'
+    AIR_HUMIDITY = 'air_humidity'
     WATER_TEMPERATURE = 'water_temperature'
-    HUMIDITY = 'humidity'
     RECIPE_START = 'recipe_start'
     RECIPE_END = 'recipe_end'
--- openag/brain/modules/ds18b20.py
+++ openag/brain/modules/ds18b20.py
@@ -5,14 +5,16 @@ class Ds18b20(Module):
     water_temperature = Output(EnvironmentalVariable.WATER_TEMPERATURE)
 
     def init(self, board_id: ReferenceParameter(DbName.MODULE, "ID of the "
-            "module for the board to which this sensor is connected"), temp_id:
-            StringParameter("CBAN id of the temperature point"), update_interval:
-            "Frequency with which to poll this sensor" = 1):
+            "module for the board to which this sensor is connected"),
+            sensor_id: StringParameter("CBAN id of the temperature point"),
+            update_interval: "Frequency with which to poll this sensor" = 1):
         self.board = self.ask(board_id)
-        self.temp_id = temp_id
+        self.sensor_id = sensor_id
         self.update_interval = update_interval
 
     def run(self):
         while True:
-            self.water_temperature.emit(self.board.cban_get(self.temp_id))
+            self.water_temperature.emit(self.board.cban_get(
+                self.sensor_id, EnvironmentalVariable.WATER_TEMPERATURE.value
+            ))
             gevent.sleep(self.update_interval)
