3cffef842a177bd2d782ac6459150d7e8cc144b8
Leon Chambers <leon8556@gmail.com>
2016-06-03 15:50:30 -0400

    Updated modules.xml file based on the modules that have been implemented

--- src/openag_brain/_design/environmental_data_point/views/latest/reduce.js
+++ src/openag_brain/_design/environmental_data_point/views/latest/reduce.js
@@ -2,7 +2,7 @@ function (key, values, rereduce) {
   var best = values[0];
   var curr_val;
   for (var i in values) {
-    var curr_val = values[i];
+    curr_val = values[i];
     if (curr_val.timestamp > best.timestamp) {
       best = curr_val;
     }
