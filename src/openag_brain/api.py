4bc60edd27091052011761bd90add9f52aba6b79
Leon Chambers <leon8556@gmail.com>
2016-06-06 10:00:27 -0400

    Added dependencies to package.xml and cleaned up API responses

--- src/openag_brain/api.py
+++ src/openag_brain/api.py
@@ -5,34 +5,35 @@ from flask import Flask, jsonify, request
 API_VER = '0.0.1'
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/api/{v}/param".format(v=API_VER), methods=['GET'])
 def list_params():
-    return jsonify(rospy.get_param_names())
+    return jsonify({"results": rospy.get_param_names()})
 
 @app.route("/api/{v}/param/<path:param_name>".format(v=API_VER), methods=['GET'])
 def get_param(param_name):
     if not rospy.has_param(param_name):
-        return "No such parameter exists\n", 400
-    return str(rospy.get_param(param_name)) + '\n'
+        return jsonify({"error": "No such parameter exists"}), 400
+    return jsonify({"result": str(rospy.get_param(param_name))})
 
 @app.route("/api/{v}/param/<path:param_name>".format(v=API_VER), methods=['POST'])
 def set_param(param_name):
     if not 'value' in request.values:
-        return "No value supplied\n", 400
+        return jsonify({"error": "No value supplied"}), 400
     rospy.set_param(param_name, request.values['value'])
-    return "Success\n", 200
+    return "", 204
 
 @app.route("/api/{v}/service".format(v=API_VER), methods=['GET'])
 def list_services():
-    return jsonify(rosservice.get_service_list())
+    return jsonify({"results": rosservice.get_service_list()})
 
 @app.route("/api/{v}/service/<path:service_name>".format(v=API_VER), methods=['GET'])
 def get_service_info(service_name):
     service_name = '/' + service_name
     service_type = rosservice.get_service_type(service_name)
     if not service_type:
-        return "No such service exists\n", 404
+        return jsonify({"error": "No such service exists"}), 404
     return jsonify({
         "request_type": service_type,
         "node": rosservice.get_service_node(service_name),
@@ -49,7 +50,7 @@ def perform_service_call(service_name):
     try:
         res = rosservice.call_service(service_name, [args])[1]
     except rosservice.ROSServiceException as e:
-        return str(e) + '\n', 400
+        return jsonify({"error": str(e)}), 400
     status_code = getattr(res, 'status_code', 200)
     data = getattr(res, 'data', None)
     if data is None:
