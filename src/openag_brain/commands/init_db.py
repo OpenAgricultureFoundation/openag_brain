40d5eea9bc44157a91b42f15e0fbca9061b851c5
Leon Chambers <leon8556@gmail.com>
2016-06-14 16:47:28 -0400

    Added database parameter to launch file; fixed import in init_db

--- src/openag_brain/commands/init_db.py
+++ src/openag_brain/commands/init_db.py
@@ -1,4 +1,5 @@
 import os
+import time
 import rospkg
 import requests
 from couchdb import PreconditionFailed
--- src/openag_brain/commands/update_launch.py
+++ src/openag_brain/commands/update_launch.py
@@ -5,38 +5,51 @@ import lxml.etree as ET
 from openag_brain.models import SoftwareModuleModel
 from openag_brain.db_names import DbName
 
+def create_node(parent, pkg, type, name):
+    e = ET.SubElement(parent, 'node')
+    e.attrib['pkg'] = pkg
+    e.attrib['type'] = type
+    e.attrib['name'] = name
+    return e
+
+def create_param(parent, name, value, type):
+    e = ET.SubElement(parent, 'param')
+    e.attrib['name'] = name
+    e.attrib['value'] = value
+    e.attrib['type'] = type
+    return e
+
+def create_group(parent, ns):
+    e = ET.SubElement(parent, 'group')
+    e.attrib['ns'] = ns
+    return e
+
+def create_remap(parent, from_val, to_val):
+    e = ET.SubElement(parent, 'remap')
+    e.attrib['from'] = from_val
+    e.attrib['to'] = to_val
+
 def update_launch(server):
     db = server[DbName.SOFTWARE_MODULE]
 
     # Form a launch file from the parameter configuration
     root = ET.Element('launch')
-    api = ET.SubElement(root, 'node')
-    api.attrib['pkg'] = 'openag_brain'
-    api.attrib['type'] = 'api.py'
-    api.attrib['name'] = 'api'
-    ft = ET.SubElement(root, 'node')
-    ft.attrib['pkg'] = 'openag_brain'
-    ft.attrib['type'] = 'topic_connector.py'
-    ft.attrib['name'] = 'topic_connector'
+    create_node(root, 'openag_brain', 'api.py', 'api')
+    create_node(root, 'openag_brain', 'topic_connector.py', 'topic_connector')
+    create_param(root, 'database', server.resource.url, "str")
     groups = {None: root}
     for module_id in db:
         if module_id.startswith('_'):
             continue
         module = SoftwareModuleModel.load(db, module_id)
         if not module.environment in groups:
-            group = ET.SubElement(root, 'group')
-            group.attrib['ns'] = module.environment
+            group = create_group(root, module.environment)
         else:
             group = groups[module.environment]
         node_pkg, node_name = module.type.split(':')
-        node = ET.SubElement(group, 'node')
-        node.attrib['pkg'] = node_pkg
-        node.attrib['type'] = node_name
-        node.attrib['name'] = module.id
+        node = create_node(group, node_pkg, node_name, module.id)
         for k,v in module.mappings.items():
-            remap = ET.SubElement(node, 'remap')
-            remap.attrib['from'] = k
-            remap.attrib['to'] = v
+            create_remap(node, k, v)
         for k,v in module.parameters.items():
             param = ET.SubElement(node, 'param')
             if k.startswith('~'):
