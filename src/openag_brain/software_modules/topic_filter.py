1c8a5d8061258fb2d1155f7869a2a5d8eaa46eca
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-12-06 13:00:27 -0500

    Filter topics using naming convention vs module db
    Topic re-publishing doesn't have to know or care about database state.
    This commit makes topic filtering more loosely coupled. By inferring the
    sensor endpoints via topic name, we no longer have to care _who_ is
    publishing that topic, or _how_. We just have to care that the sensor
    topic exists.
    
    As a fringe benefit, this also reduces the # of lines.

--- src/openag_brain/software_modules/topic_filter.py
+++ src/openag_brain/software_modules/topic_filter.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python
-import time
 import rospy
-from openag.cli.config import config as cli_config
-from openag.utils import synthesize_firmware_module_info
-from openag.models import FirmwareModule, FirmwareModuleType
-from openag.db_names import FIRMWARE_MODULE, FIRMWARE_MODULE_TYPE
-from couchdb import Server
+import rosgraph
 
+from re import match
 from roslib.message import get_message_class
 
 class EWMA:
@@ -34,32 +30,37 @@ def filter_topic(src_topic, dest_topic, topic_type):
     sub = rospy.Subscriber(src_topic, topic_type, callback)
     return sub, pub
 
-def filter_all_topics(module_db, module_type_db):
-    modules = {
-        module_id: FirmwareModule(module_db[module_id]) for module_id in
-        module_db if not module_id.startswith('_')
-    }
-    module_types = {
-        type_id: FirmwareModuleType(module_type_db[type_id]) for type_id in
-        module_type_db if not type_id.startswith("_")
-    }
-    modules = synthesize_firmware_module_info(modules, module_types)
-    for module_id, module_info in modules.items():
-        for output_name, output_info in module_info["outputs"].items():
-            src_topic = "/sensors/{}/{}/raw".format(module_id, output_name)
-            dest_topic = "/sensors/{}/{}/filtered".format(
-                module_id, output_name
-            )
-            topic_type = get_message_class(output_info["type"])
-            filter_topic(src_topic, dest_topic, topic_type)
+def gen_sensor_topic_desc(pubs):
+    """
+    Generate a description dict for topics that are sensor topics.
+    Matches, filters and parses topic names to find sensor information.
+    """
+    for topic, topic_type in pubs:
+        result = match("/sensors/(\w+)/(\w+)/raw", topic)
+        if result:
+            yield {
+                "module_id": result.groups(1),
+                "output_name": result.groups(2),
+                "topic": topic,
+                "type": topic_type
+            }
+
+def filter_all_topics(pubs):
+    """
+    Given an iterator publishers, where each publisher is a two-tuple
+    `(topic, type)`, create a filtered topic endpoint.
+    """
+    for desc in gen_sensor_topic_desc(pubs):
+        src_topic_type = get_message_class(desc["type"])
+        src_topic = desc["topic"]
+        module_id = desc["module_id"]
+        output_name = desc["output_name"]
+        dest_topic = "/sensors/{}/{}/filtered".format(module_id, output_name)
+        filter_topic(src_topic, dest_topic, src_topic_type)
 
 if __name__ == '__main__':
     rospy.init_node("topic_filter")
-    db_server = cli_config["local_server"]["url"]
-    if not db_server:
-        raise RuntimeError("No local server specified")
-    server = Server(db_server)
-    module_db = server[FIRMWARE_MODULE]
-    module_type_db = server[FIRMWARE_MODULE_TYPE]
-    filter_all_topics(module_db, module_type_db)
+    rostopic_master = rosgraph.Master("/rostopic")
+    pubs, subs, _ = rostopic_master.getSystemState()
+    filter_all_topics(pubs)
     rospy.spin()
