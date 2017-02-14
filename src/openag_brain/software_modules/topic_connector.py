e4556c8aa8b2de7c6d8b7743946cc1182679ebd8
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-02-13 16:54:11 -0800

    Read topics from JSON

--- src/openag_brain/software_modules/topic_connector.py
+++ src/openag_brain/software_modules/topic_connector.py
@@ -10,21 +10,15 @@ of hardware acts (e.g. /environment_1/measured/air_temperature). This module
 connects topics so as to ensure that both of these system views work as
 expected. There should be exactly one instance of this module in the system
 """
-import sys
-import time
 import rospy
 import rosgraph
 import rostopic
-from openag.cli.config import config as cli_config
-from openag.utils import synthesize_firmware_module_info
+import json
+from openag.utils import synthesize_firmware_module_info, merge_deep
 from openag.models import FirmwareModule, FirmwareModuleType
 from openag.db_names import FIRMWARE_MODULE, FIRMWARE_MODULE_TYPE
-from couchdb import Server
 from std_msgs.msg import Bool, Float32, Float64
-
-from openag_brain import params
 from openag_brain.msg import SensorInfo
-from openag_brain.srv import Empty
 from roslib.message import get_message_class
 
 def connect_topics(
@@ -52,14 +46,26 @@ def connect_sensor_info_topics(src_topic, dest_topic):
     pub = rospy.Publisher(dest_topic, SensorInfo, queue_size=1, latch=True)
     sub = rospy.Subscriber(src_topic, SensorInfo, pub.publish)
 
-def connect_all_topics(module_db, module_type_db):
+def connect_all_topics(inputs_map, outputs_map):
+    """
+    Connect sensor topics and actuator topics through environmental variables.
+    We connect keys to values in `connection_map`.
+    """
+    for sensor_topic, measured_topic in inputs_map:
+        connect_topics()
+
+
+def connect_all_topics(modules):
+    module_list = modules[FIRMWARE_MODULE]
+    module_type_list = modules[FIRMWARE_MODULE_TYPE]
+
     modules = {
-        module_id: FirmwareModule(module_db[module_id]) for module_id in
-        module_db if not module_id.startswith('_')
+        module_id: FirmwareModule(module_list[module_id]) for module_id in
+        module_list if not module_id.startswith('_')
     }
     module_types = {
-        type_id: FirmwareModuleType(module_type_db[type_id]) for type_id in
-        module_type_db if not type_id.startswith("_")
+        type_id: FirmwareModuleType(module_type_list[type_id]) for type_id in
+        module_type_list if not type_id.startswith("_")
     }
     modules = synthesize_firmware_module_info(modules, module_types)
     for module_id, module_info in modules.items():
@@ -99,11 +105,10 @@ def connect_all_topics(module_db, module_type_db):
 
 if __name__ == '__main__':
     rospy.init_node("topic_connector")
-    db_server = cli_config["local_server"]["url"]
-    if not db_server:
-        raise RuntimeError("No local server specified")
-    server = Server(db_server)
-    module_db = server[FIRMWARE_MODULE]
-    module_type_db = server[FIRMWARE_MODULE_TYPE]
-    connect_all_topics(module_db, module_type_db)
+    module_files = rospy.get_param("~module_files")
+    modules_json = (
+        merge_deep(json.load(file) for file in module_files)
+        if len(module_files) else {}
+    )
+    connect_all_topics(modules_json)
     rospy.spin()
