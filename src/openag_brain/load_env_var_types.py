6ead576f14109030433e26d78904604f7454672d
Bob Davis <bob.davis@fenome.com>
2017-04-06 17:38:38 -0600

    added utility function to store environmental var types and load them

---
+++ src/openag_brain/load_env_var_types.py
@@ -0,0 +1,32 @@
+import rospy
+
+
+class VariableInfo(dict):
+    items = {}
+
+    def __init__(self, name, description, units=None):
+        self.name = name
+        self.__doc__ = description
+        self.units = units
+
+    def __str__(self):
+        return self.name
+
+# env_var = rospy.get_param('/environment_variables')
+# recipe_var = rospy.get_param('/recipe_variables')
+# camera_var = rospy.get_param('/camera_variables')
+# user_var = rospy.get_param('/user_variables')
+
+def create_variables(var_dict):
+    """
+    Converts each dictionary item into the VariableInfo class to allow for them
+    to be referenced by name directly without having to look up the name key.
+    For example, print(variable) instead of print(variable['name'])
+    :param: var_dict : dictionary with each value being a dictionary containing
+        name, description, units
+    """
+    variables = []
+    for item in var.items.keys():
+        variable = VariableInfo(item['name'], var['description'], var['units'])
+        variables.append(variable)
+    return tuple(variables)
