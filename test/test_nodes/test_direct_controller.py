48dcab56b95fde0aa1a8d43b62201b2efb6e5129
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-04-13 04:15:39 +0000

    added logging to test to help troubleshoot

--- test/test_nodes/test_direct_controller.py
+++ test/test_nodes/test_direct_controller.py
@@ -17,22 +17,24 @@ class TestDirectController(unittest.TestCase):
     def setUp(self):
         self.namespace = "/tests/direct_controller/mock_controller"
         self.pub_desired = rospy.Publisher("{}/desired".format(self.namespace),
-                                            Float64, queue_size=None)
+                                            Float64, queue_size=10)
         self._received = []
 
     def callback(self, item):
         self._received.append(item)
 
     def test_echo_topic(self):
+        rospy.init_node(NAME, anonymous=True)
         msgs = [1, 3, 0, 40, 30, 7]
         self.sub_measured = rospy.Subscriber("{}/measured".format(self.namespace),
                                             Float64, self.callback)
         for msg in msgs:
-            print(msg)
+            rospy.loginfo("Published: {:f}".format(msg))
             self.pub_desired.publish(msg)
-        rospy.sleep(5)
-        print(_received)
-        self.assertTrue(all(msgs == self._received))
+        rospy.sleep(3)
+        rospy.loginfo("Received: " + ' '.join(self._received) + ".")
+        rospy.loginfo("There were " + str(len(self._received)) + " messages received.")
+        self.assertTrue(msgs == self._received)
 
 
 if __name__ == "__main__":
