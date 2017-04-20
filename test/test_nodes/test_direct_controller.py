f3c1604fdcab1dfb184749a71fea952f4e14b680
Bob Davis <bob.davis@fenome.com>
2017-04-20 15:45:43 -0600

    modified test order

--- test/test_nodes/test_direct_controller.py
+++ test/test_nodes/test_direct_controller.py
@@ -15,28 +15,53 @@ class TestDirectController(unittest.TestCase):
     correctly.
     """
     def setUp(self):
-        self.namespace = "/tests/direct_controller/mock_controller"
-        self.pub_desired = rospy.Publisher("{}/desired".format(self.namespace),
-                                            Float64, queue_size=10)
+        self.namespace = "mock_controller"
+
+        self.msgs = [1, 3, 0, 40, 30, 7]
         self._received = []
 
     def callback(self, item):
+        rospy.loginfo("I heard %s", item)
         self._received.append(item)
+        self.assertTrue(self.msgs[:len(self._received)] == self._received)
 
     def test_echo_topic(self):
         rospy.init_node(NAME, anonymous=True)
-        msgs = [1, 3, 0, 40, 30, 7]
+        self.pub_desired = rospy.Publisher("{}/desired".format(self.namespace),
+                                                    Float64, queue_size=10)
+        # rospy.sleep(10)
         self.sub_measured = rospy.Subscriber("{}/measured".format(self.namespace),
                                             Float64, self.callback)
+        # self.sub_commanded = rospy.Subscriber("{}/commanded".format(self.namespace),
+                                            # Float64, self.callback)
         for msg in msgs:
             rospy.loginfo("Published: {:f}".format(msg))
-            self.pub_desired.publish(msg)
-        rospy.sleep(3)
-        rospy.loginfo("Received: " + ' '.join(self._received) + ".")
+            self.pub_desired.publish(self.msg)
+            rospy.sleep(1)
+        # rospy.sleep(3)
+        #
+        rospy.spin() # Run until all messages are received.
+        #self.assertTrue(msgs == self._received)
+        rospy.loginfo("Received: " + ' '.join(self._received))
         rospy.loginfo("There were " + str(len(self._received)) + " messages received.")
-        self.assertTrue(msgs == self._received)
+        rospy.loginfo("New Message")
+def publisher_node():
+    pass
+
+def listener_node():
+
+    def callback(item):
+        print("I Heard {}".format(item))
+    namespace = "/tests/mock_controller"
+    rospy.init_node(NAME, anonymous=True)
+    sub_measured = rospy.Subscriber("{}/measured".format(namespace),
+                                        Float64, callback)
+    sub_commanded = rospy.Subscriber("{}/commanded".format(namespace),
+                                        Float64, callback)
+    rospy.spin()
 
 
 if __name__ == "__main__":
     import rostest
     rostest.rosrun(PKG, NAME, TestDirectController)
+    # listener_node()
