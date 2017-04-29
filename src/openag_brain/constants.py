6f0e0e6746ef0d669bd99d40b277ac6c4190dd02
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-04-29 00:55:18 +0000

    Avoid touching sentinal values in EWMA filter

--- src/openag_brain/constants.py
+++ src/openag_brain/constants.py
@@ -8,3 +8,10 @@ We use it to signal to the controller that the feedback loop should
 be broken.
 """
 NULL_SETPOINT_SENTINAL = -9000000009.00001
+
+"""
+The set of all sentinals. Used to check if a number is any sentinal.
+"""
+SENTINALS = frozenset((
+    NULL_SETPOINT_SENTINAL
+))
