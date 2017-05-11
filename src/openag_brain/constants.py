a4be8cb3e3b96021b71003566b40da1cf918c3c8
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-05-11 13:33:29 -0700

    Add trailing comma, making () a tuple
    Oops... one of Python's syntactic footguns.

--- src/openag_brain/constants.py
+++ src/openag_brain/constants.py
@@ -15,5 +15,6 @@ NULL_SETPOINT_SENTINEL = -9000000009.00001
 The set of all sentinel. Used to check if a number is any kind of sentinel.
 """
 SENTINELS = frozenset((
-    NULL_SETPOINT_SENTINEL
+    # Don't forget the trailing comma (making this a tuple not a grouping)
+    NULL_SETPOINT_SENTINEL,
 ))
