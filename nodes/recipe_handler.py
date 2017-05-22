f36ae27c8c8a52608e507343d74f9b3eff4080f0
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-05-22 17:13:27 -0400

    Set param with supported formats

--- nodes/recipe_handler.py
+++ nodes/recipe_handler.py
@@ -190,7 +190,7 @@ class RecipeHandler:
         rospy.Service(services.STOP_RECIPE, Empty, self.stop_recipe_service)
         rospy.set_param(
             params.SUPPORTED_RECIPE_FORMATS,
-            ','.join(interpret_recipe.methods.keys())
+            ','.join(RECIPE_INTERPRETERS.keys())
         )
         return self
 
