b61f1a09113cfd7bf735c94c3844e3169af53960
Gordon <gordonbrander@users.noreply.github.com>
2017-06-01 13:43:41 -0600

    added phased_dense recipe interpreter

--- src/openag_brain/recipe_interpreters.py
+++ src/openag_brain/recipe_interpreters.py
@@ -1,6 +1,7 @@
 import rospy
 from openag_brain.load_env_var_types import VariableInfo
 from openag_brain.utils import trace
+from datetime import datetime
 
 RECIPE_START = VariableInfo.from_dict(
     rospy.get_param('/var_types/recipe_variables/recipe_start'))
@@ -55,3 +56,225 @@ def interpret_simple_recipe(recipe, start_time, now_time):
         (variable, value)
         for variable, value in state.iteritems()
     )
+
+
+def phased_dense_calc_phase(phase_dict):
+    """
+    Determines the current state given the start_time and now_time for a phase
+    """
+    return state
+
+
+
+def interpret_recipe_started_verification(recipe, start_time, now_time):
+    """
+    Produces a tuple of ``(variable, value)`` pairs by building up
+    a recipe state from walking through the recipe keyframes
+    """
+    _id = recipe["_id"]
+    phases = recipe["phases"]
+    rospy.loginfo(phases)
+    end_time_relative = phases[-1][0]
+    trace("recipe_handler: interpret_simple_recipe end_time_relative=%s",
+        end_time_relative)
+    end_time = start_time + end_time_relative
+    # If start time is at some point in the future beyond the threshold
+    if start_time - now_time > THRESHOLD:
+        raise ValueError("Recipes cannot be scheduled for the future")
+    # If there are no recipe operations, immediately start and stop
+    # The recipe.
+    if not len(phases):
+        return (
+            (RECIPE_START.name, _id),
+            (RECIPE_END.name, _id),
+        )
+    if now_time >= (end_time + THRESHOLD):
+        return ((RECIPE_END.name, _id),)
+    if abs(now_time - start_time) < THRESHOLD:
+        return ((RECIPE_START.name, _id),)
+
+    now_relative = (now_time - start_time)
+
+    # Create a state object to accrue recipe setpoint values.
+    state = {}
+    # Build up state up until now_time (inclusive).
+    trace("recipe_handler: interpret_simple_recipe now=%s", now_relative)
+    for timestamp, variable, value in operations:
+        if timestamp > now_relative:
+            break
+        state[variable] = value
+        trace("recipe_handler: interpret_simple_recipe: %s %s %s",
+            timestamp, variable, value)
+    rospy.loginfo(state)
+    return tuple(
+        (variable, value)
+        for variable, value in state.iteritems()
+    )
+
+
+def interpret_phased_dense_recipe(recipe, current_time, start_time):
+    """
+    Recipe Interpreter should read a recipe, current_time, start_time, variable and return a value.
+       Determine the time since the beginning of the recipe.
+       Determine what is the current step
+       Calculate the remaining time left in this step.
+       Look up the value within that step for that variable.
+    """
+
+    # Returns a list of the phases and step durations  [(duration_of_phase_1, duration_of_step_1), (duration_of_phase_2, duration_of_step_2), etc]
+    duration_of_phases_steps = calc_duration_of_phases_steps(recipe['phases'])
+    current_phase_number, duration_in_step = calc_phase_and_time_remaining(duration_of_phases_steps,
+                                                                           current_time,
+                                                                           start_time)
+    current_phase = recipe['phases'][current_phase_number]
+    state = {}
+    for variable, variable_step_data in current_phase['step'].items():
+        value = determine_value_for_step(variable_step_data, duration_in_step)
+        state[variable] = value
+    return tuple(
+        (variable, value)
+        for variable, value in state.iteritems()
+    )
+
+
+
+def determine_value_for_step(variable_step_data, duration_in_step):
+    """
+    variable_step_data = [{"start_time": 0, "end_time": 6, "value": 20},
+                          {"start_time": 6, "end_time": 18, "value": 23},
+                          {"start_time": 18, "end_time": 24, "value": 19}]
+    duration_in_step = 15
+    Given the time within a step, what value is expected.
+    """
+
+    for row in variable_step_data:
+        if row['start_time'] <= duration_in_step <= row['end_time']:
+            return row['value']
+
+
+def calculate_max_duration_from_step(step):
+    """
+    Determines the total duration of this step. Normally it is 24 hours.
+       Could add other validation steps here as well.
+       Convert to numpy and use argmax
+    """
+    max_time = 0
+    for variable, start_end_times in step.items():
+        for data in start_end_times:
+            if data['start_time'] > data['end_time']:
+                raise Exception("Start_time is after end time.")
+            elif max_time < max(data['start_time'], data['end_time']):
+                max_time = max(data['start_time'], data['end_time'])
+    return max_time
+
+
+def calc_duration_of_phases_steps(phases):
+    """
+    Returns a list with the duration of the step and the entire phase
+    """
+    duration_of_phases_steps = []
+    for phase in phases:
+        cycles = phase['cycles']
+        max_duration = calculate_max_duration_from_step(phase['step'])
+        duration_of_phases_steps.append((cycles*max_duration, max_duration))
+    return duration_of_phases_steps
+
+
+def calc_phase_and_time_remaining(duration_of_phases_steps, current_time, start_time):
+    """
+    duration_of_phases_steps == [(336, 24), (480, 24), (168, 24)]
+    current_time : datetime : UTC of the current time
+    start_time : datetime : UTC time the recipe started
+    return: current_phase_number, duration_in_phase
+    """
+    time_since_start = current_time - start_time
+    time_since_start = time_since_start.total_seconds() / 3600
+
+    time_remaining = time_since_start
+    for i, (total_duration, step_duration) in enumerate(duration_of_phases_steps):
+        if time_remaining > total_duration:
+            time_remaining -= total_duration
+        else:
+            duration_in_phase = time_remaining % step_duration
+            current_phase_number = i
+            break
+    return current_phase_number, duration_in_phase
+
+
+def test_calc_phase_and_time_remaining():
+    duration_of_phases_steps = [(336, 24), (480, 24), (168, 24)]
+    start_time = datetime.strptime("2017-04-17 14:00", "%Y-%m-%d %H:%S")
+    current_time = datetime.strptime("2017-04-18 20:00", "%Y-%m-%d %H:%S")
+    current_phase_number, duration_in_step = calc_phase_and_time_remaining(duration_of_phases_steps,
+                                           current_time, start_time)
+    assert (current_phase_number, duration_in_step) == (0, 6)
+
+    start_time = datetime.strptime("2017-04-17 14:00", "%Y-%m-%d %H:%S")
+    current_time = datetime.strptime("2017-04-18 03:00", "%Y-%m-%d %H:%S")
+    current_phase_number, duration_in_step = calc_phase_and_time_remaining(duration_of_phases_steps,
+                                           current_time, start_time)
+    print(current_phase_number, duration_in_step)
+    assert (current_phase_number, duration_in_step) == (0, 13)
+
+
+def test_calculate_max_duration_from_step():
+    step = { "air_temperature": [{"start_time": 0, "end_time": 6, "value": 20},
+                                  {"start_time": 6, "end_time": 18, "value": 23},
+                                  {"start_time": 18, "end_time": 24, "value": 19}],
+              "nutrient_flora_duo_a": [{"start_time": 0, "end_time": 6, "value": 5},
+                                       {"start_time": 6, "end_time": 18, "value": 2},
+                                       {"start_time": 18, "end_time": 24, "value": 5}],
+              "nutrient_flora_duo_b": [{"start_time": 0, "end_time": 6, "value": 2}],
+              "light_illuminance": [{"start_time": 0, "end_time": 6, "value": 4},
+                                    {"start_time": 18, "end_time": 24, "value": 3}],
+        }
+    max_time = calculate_max_duration_from_step(step)
+    assert max_time == 24
+
+
+def test_calc_duration_of_phases_steps():
+    phases = [    # Previously operations,  Needs to be an ordered dictionary.
+          { "name": "early",
+            "cycles": 14,    # Add check for duration of a step to be a total of 24 hours. (Not a necessarity but valuable for consistency/simplicity)
+            "step": { "air_temperature": [{"start_time": 0, "end_time": 6, "value": 20},
+                                          {"start_time": 6, "end_time": 18, "value": 23},
+                                          {"start_time": 18, "end_time": 24, "value": 19}],
+                      "nutrient_flora_duo_a": [{"start_time": 0, "end_time": 6, "value": 5},
+                                               {"start_time": 6, "end_time": 18, "value": 2},
+                                               {"start_time": 18, "end_time": 24, "value": 5}],
+                      "nutrient_flora_duo_b": [{"start_time": 0, "end_time": 6, "value": 2}],
+                      "light_illuminance": [{"start_time": 0, "end_time": 6, "value": 4},
+                                            {"start_time": 18, "end_time": 24, "value": 3}],
+                },
+          },
+          { "name": "middle",
+            "cycles": 20,
+            "step": { "air_temperature": [{"start_time": 0, "end_time": 6, "value": 20},
+                                          {"start_time": 6, "end_time": 18, "value": 23},
+                                          {"start_time": 18, "end_time": 24, "value": 19}],
+                      "nutrient_flora_duo_a": [{"start_time": 6, "end_time": 18, "value": 1.4}],
+                      "nutrient_flora_duo_b": [{"start_time": 6, "end_time": 18, "value": 0.7}],
+                      "light_illuminance": [{"start_time": 6, "end_time": 18, "value": 1}],
+                },
+          },
+          { "name": "late",
+            "cycles": 7,
+            "step": { "air_temperature": [{"start_time": 6, "end_time": 24, "value": 23}],
+                    },
+          }
+        ]
+    duration_of_phases_steps = calc_duration_of_phases_steps(phases)
+    assert duration_of_phases_steps == [(336, 24), (480, 24), (168, 24)]
+
+
+def test_determine_value_for_step():
+    variable_step_data = [{"start_time": 0, "end_time": 6, "value": 20},
+                          {"start_time": 6, "end_time": 18, "value": 23},
+                          {"start_time": 18, "end_time": 24, "value": 19}]
+    duration_in_step = 15
+    value = determine_value_for_step(variable_step_data, duration_in_step)
+    assert value == 23
+    duration_in_step = 0
+    value = determine_value_for_step(variable_step_data, duration_in_step)
+    assert value == 20
+
--- test/test_src/test_recipe_interpreter.py
+++ test/test_src/test_recipe_interpreter.py
@@ -4,27 +4,44 @@ PKG="openag_brain"
 import sys, os
 import unittest
 from time import time
+from datetime import datetime
+import json
 
-from openag_brain.recipe_interpreters import interpret_simple_recipe
+DIR_NAME = os.path.dirname(__file__)
+sys.path.append(os.path.join(DIR_NAME, '../..'))
+from openag_brain.recipe_interpreters import interpret_simple_recipe, interpret_phased_dense_recipe
+from test.data.mock_recipes import phased_dense, MOCK_RECIPE_A
 
 
-MOCK_RECIPE_A = {
-            "format": "simple",
-            "version": "0.0.1",
-            "operations": [
-                            [0, "air_temperature", 24],
-                            [0, "water_temperature", 22]
-                          ]
-            }
+def load_recipe_from_file(file_name):
+    with open(file_name, 'r') as f:
+        recipe = json.load(f)
+    return recipe
+
+
+def _test_load_recipe_from_file():
+    recipe = load_recipe_from_file(os.path.join(DIR_NAME, '../../test/data/mock_recipe_v3.json'))
+    print(recipe)
+    assert type(recipe) == dict
+    assert False
+
 
 class TestRecipeInterpreter(unittest.TestCase):
 
-    def test_interpret_simple_recipe():
+    def test_interpret_simple_recipe(self):
         now_time = time()
-        start_time = now_time - 100
-        setpoints = interpet_simple_recipe(MOCK_RECIPE_A, start_time, now_time)
-        print("Recipe Handler returns expected number of setpoints")
+        start_time = now_time - 10
+        setpoints = interpret_simple_recipe(MOCK_RECIPE_A, start_time, now_time)
         assert len(setpoints) == 2
+  
+
+    def test_interpret_phased_dense_recipe(self):
+        #now_time = time()
+        #start_time = now_time - 10
+        start_time = datetime.strptime("2017-04-17 14:00", "%Y-%m-%d %H:%S")
+        now_time = datetime.strptime("2017-04-18 20:00", "%Y-%m-%d %H:%S")
+        setpoints = interpret_phased_dense_recipe(phased_dense, start_time, now_time)
+        assert len(setpoints) == 4
 
 
 if __name__ == '__main__':
